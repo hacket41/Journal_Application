e56c3c9bc07f82d3726176cf65f86a3abe104406
Abhijit Singh <abhijit.singh.0106@gmail.com>
2025-12-19 08:17:33 -0800

    Commit before SQLITEMigrations

---
+++ Models/JournalEntry.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JournalApp.Models
+{
+    public class JournalEntry
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+        public DateTime EntryDate { get; set; }
+
+        public string? Title { get; set; }
+
+        [Required]
+        public string Content { get; set; } = string.Empty;
+
+        [Required]
+        public string PrimaryMood { get; set; } = "Neutral";
+
+        public string? SecondaryMoods { get; set; } 
+        public string? Category { get; set; }
+        public string? Tags { get; set; } 
+    }
+}
